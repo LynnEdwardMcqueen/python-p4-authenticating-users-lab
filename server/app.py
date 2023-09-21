5b81d5f17eb5f09b9e6950bdb60b2db275e28b58
Lynn McQueen <lynn.edward.mcqueen@gmail.com>
2023-09-20 20:34:58 -0600

    Misunderstanding with delete/clear differences, but it works now

--- server/app.py
+++ server/app.py
@@ -17,10 +17,35 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+test_array = []
 
+class CheckSession(Resource):
+    def get(self):
+        test_array.append(0)
+        if session.get('user_id'):
+            test_array.append(1)
+            user_id = session.get("user_id")
+            user_dict = User.query.filter(User.id == user_id).first().to_dict()
+            # Only wants user information, not the articles.
+            del user_dict["articles"]
+            response = make_response(
+                jsonify(user_dict),
+                200
+            )
+        else:
+            test_array.append(2)
+            response = make_response(
+                {},
+                401
+            )
+        return response
+    
 class ClearSession(Resource):
 
     def delete(self):
+
+        test_array.append(3)
+        print("Deleting the session;  Logging out.")
     
         session['page_views'] = None
         session['user_id'] = None
@@ -32,6 +57,35 @@ class IndexArticle(Resource):
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
+
+class Login(Resource):
+    
+    def post(self):
+        test_array.append(4)
+        # The http request body is in the form {username: user_name}
+        login_data = request.get_json()
+  
+        user = User.query.filter(User.username == login_data["username"]).first().to_dict()
+
+        session['user_id'] = user["id"]
+    
+        response_dict = {"username" : user["username"], "id" : user["id"]}
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+        return response
+
+class Logout(Resource):
+    def delete(self):
+        test_array.append(5)
+        print("Deleting the session;  Logging out.")
+    
+        session['page_views'] = None
+        session['user_id'] = None
+
+        return {}, 204
 
 class ShowArticle(Resource):
 
@@ -48,9 +102,13 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+
 
 
 if __name__ == '__main__':
